675425b9aed7757a831d8a1f2f7d0ab9bf1fa620
Chetan9850 <chetancdalawaimath@gmail.com>
2024-07-25 22:41:10 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -1,45 +1,8 @@
-#include <stdio.h>
-#include <ctype.h>
-#include <string.h>
+#ifndef SOUNDEX_H
+#define SOUNDEX_H
 
+#include <string>
 
- 
-char getSoundexCode(char c) {
-    static const char soundexCodes[] = {
-        '0', '1', '2', '3', '0', '1', '2', '0', '0', '2', '2', '4', '5', '5', '0', '1', '2', '6', '2', '3', '0', '1', '0', '2', '0', '2'
-    };
- 
-    c = toupper(c);
-    if (c >= 'A' && c <= 'Z') {
-        return soundexCodes[c - 'A'];
-    }
-    return '0';
-}
- 
-char fetch_firstchar(const std::string& name) {
-    if (name.empty()) return '\0';
-    return toupper(name[0]);
-}
- 
-void padSoundex(char* soundex) {
-    size_t len = strlen(soundex);
-    while (len < 4) {
-        soundex[len++] = '0';
-    }
-    soundex[4] = '\0';
-}
+std::string generateSoundex(const std::string& name);
 
- 
-void initializeSoundex(char* soundex, char firstChar) {
-    soundex[0] = toupper(firstChar);
-void generateSoundex(const char* name, char* soundex) {
-    initializeSoundex(soundex, name[0]); // Initialize soundex with the first character of name
-    char prevCode = getSoundexCode(name[0]); // Get Soundex code for the first character
- 
-    size_t i = 1;
-    while (name[i] != '\0' && i < 4) {
-        prevCode = eliminateZeroAndRepeatedValue(prevCode, soundex, i, name);
-        i++;
-    }
-    padSoundex(soundex);
-}
+#endif // SOUNDEX_H
